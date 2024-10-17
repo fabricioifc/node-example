0ef1a14c046459d67a51638399e7745ce20b3c69
IFC VIDEIRA <ifc.videira@ifc.edu.br>
2024-10-17 16:07:05 -0300

    criei uma soma em loop

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-const express = require("express");
+var numeros = [1,4,12,21,10,13]
 
-// Create an Express app and listen for incoming requests on port 3000
-const app = express();
-const port = process.env.PORT || 3000;
+var soma = 0
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i]
+}
 
-// Use middleware to parse incoming requests with JSON and URL-encoded payloads
-app.use(express.json());
-app.use(express.urlencoded());
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Internal Server Error");
-});
-
-// Handle GET requests to the root URL
-router.get("/", (req, res) => {
-  res.send("Welcome to the Webhook Server!");
-});
-
-// Handle POST requests to specific URLs i.e. webhook endpoints
-router.post("/webhook-1", (req, res) => {
-  console.log(req.body);
-  res.send("Webhook 1 successfully received.");
-});
-
-router.post("/webhook-2", (req, res) => {
-  console.log(req.body);
-  res.send("Webhook 2 successfully received.");
-});
-
-// Mount the router middleware
-app.use(router);
-
-// Start the server and listen for incoming connections
-app.listen(port, () => {
-  console.log(`Server running at https://localhost:${port}/`);
-});
+console.log(soma)
